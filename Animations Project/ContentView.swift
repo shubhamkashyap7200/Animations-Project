5c2e588a1f118bd19b119bc7db728361a88b56b2
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-10-14 15:41:28 +0530

    1st Commit

--- Animations Project/ContentView.swift
+++ Animations Project/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: Properties
+    @State private var animationAmount = 1.0
+    
+    
+    // MARK: Body
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack() {
+            Button("Single") {
+                // do nothing
+//                animationAmount += 1
+            }
+            .padding(50)
+            .background(.blue)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .overlay {
+                Circle()
+                    .stroke(.blue)
+                    .scaleEffect(animationAmount)
+                    .opacity(2 - animationAmount)
+                    .animation(
+                        .easeInOut(duration: 1)
+                        .repeatForever(autoreverses: false)
+                        , value: animationAmount)
+            }
+            .onAppear {
+                animationAmount = 2
+            }
         }
-        .padding()
+        
     }
 }
 
