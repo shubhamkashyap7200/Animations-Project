927189f9a544fba47ea8b0168c92a4e11dea7eba
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-10-14 15:25:38 +0530

    Initial Commit

---
+++ Animations Project/Animations_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animations_ProjectApp.swift
+//  Animations Project
+//
+//  Created by Shubham on 10/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Animations_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animations Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Animations Project
+//
+//  Created by Shubham on 10/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
