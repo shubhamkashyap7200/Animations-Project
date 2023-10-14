e9569bf220dc7d0679bac1f014792568a1c314c6
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-10-14 18:47:07 +0530

    more chnagaes

--- Animations Project/ContentView.swift
+++ Animations Project/ContentView.swift
@@ -9,15 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
     // MARK: Properties
-    @State private var animationAmount = 1.0
-    
+    let letters = Array("Hello Swift")
+    @State private var dragAmount = CGSize.zero
+    @State private var enabled = false
     
     // MARK: Body
+    var body: some View {
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count, id: \.self) { num in
+                Text(String(letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? .blue : .red)
+                    .offset(dragAmount)
+                    .animation(
+                        .default.delay(Double(num) / 20)
+                        , value: dragAmount
+                    )
+            }
+        }
+        .gesture(
+            DragGesture()
+                .onChanged { dragAmount = $0.translation}
+                .onEnded { _ in
+                    dragAmount = .zero
+                    enabled.toggle()
+                }
+        )
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+
+
+
+
+struct PulsatingButton: View {
+    @State private var animationAmount = 1.0
+    
     var body: some View {
         VStack() {
             Button("Single") {
                 // do nothing
-//                animationAmount += 1
+                //                animationAmount += 1
             }
             .padding(50)
             .background(.blue)
@@ -41,6 +78,26 @@ struct ContentView: View {
     }
 }
 
-#Preview {
-    ContentView()
+struct RotationEffectButton: View {
+    @State private var animationAmount = 0.0
+    
+    var body: some View {
+        // MARK: Body
+        VStack {
+            Button("Tap Me") {
+                withAnimation {
+                    animationAmount += 180
+                }
+            }
+            .padding(50)
+            .frame(width: 300, height: 200)
+            .background(Color.indigo)
+            .foregroundStyle(.white)
+            .border(Color.black, width: 4.0)
+            .clipShape(RoundedRectangle(cornerRadius: 8.0))
+            .shadow(radius: 6.0)
+            .rotation3DEffect(.degrees(animationAmount),axis: (x: 1.0, y: 0.0, z: 0.0))
+        }
+        
+    }
 }
